ba8a7f86c782fcc90a6bf71f074462ec782adb09
giordanv2 <95988263+giordanv2@users.noreply.github.com>
2024-11-22 02:18:21 -0700

    feat: make the app compile

--- app/src/main/java/com/example/tumovieapp/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/tumovieapp/data/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.tumovieapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [MovieEntity::class], version = 1)
+@Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
 }
